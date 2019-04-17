47ae78be180958c2aaaa3b0a14489961474f7cfe
DmitriyFF Smile <dmitriyff@gmail.com>
2019-04-17 20:13:54 +0700

    feat(ide): Add revert command for one file
    DEV-274

--- dist/src/commands/revert.js
+++ dist/src/commands/revert.js
@@ -1,8 +1,34 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// @ts-ignore
-function revert(...args) {
-    console.log('command:revert', ...args);
+const atom_1 = require("atom");
+const utils_1 = require("../utils");
+const state_1 = require("../state");
+async function revert(event) {
+    let target = event.target;
+    if (target.nodeName === 'LI') {
+        target = target.firstChild;
+    }
+    const path = target.getAttribute('data-path');
+    if (!path) {
+        throw new Error('No data-path');
+    }
+    const module = utils_1.getModuleByPath(path);
+    if (!module) {
+        throw new Error('Error get module');
+    }
+    const { modules } = state_1.default.getValue();
+    const content = modules[module].content;
+    const file = new atom_1.File(path);
+    try {
+        await file.write(content || '');
+    }
+    catch (err) {
+        throw new Error('Error write to file');
+    }
+    state_1.default.next(Object.assign({}, state_1.default.getValue(), { modules: Object.assign({}, modules, { [module]: {
+                content,
+                modified: false
+            } }) }));
 }
 exports.revert = revert;
 //# sourceMappingURL=revert.js.map
--- src/commands/revert.ts
+++ src/commands/revert.ts
@@ -1,4 +1,48 @@
-// @ts-ignore
-export function revert(...args) {
-    console.log('command:revert', ...args);
+import { File } from 'atom';
+
+import {
+    getModuleByPath
+} from '../utils';
+
+import { default as __state } from '../state';
+
+export async function revert(event: CustomEvent) {
+    let target: HTMLElement = event.target as HTMLElement;
+
+    if (target.nodeName === 'LI') {
+        target = target.firstChild as HTMLElement;
+    }
+
+    const path = target.getAttribute('data-path');
+
+    if (!path) {
+        throw new Error('No data-path');
+    }
+
+    const module = getModuleByPath(path);
+    if (!module) {
+        throw new Error('Error get module');
+    }
+
+    const { modules } = __state.getValue();
+
+    const content = modules[module].content;
+
+    const file = new File(path);
+    try {
+        await file.write(content || '');
+    } catch (err) {
+        throw new Error('Error write to file');
+    }
+
+    __state.next({
+        ...__state.getValue(),
+        modules: {
+            ...modules,
+            [module]: {
+                content,
+                modified: false
+            }
+        }
+    });
 }
