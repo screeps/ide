362ac65d8c36d163126cb186032afbae799ff0c0
DmitriyFF Smile <dmitriyff@gmail.com>
2019-05-08 20:44:43 +0700

    feat(ide): Remove all items from status bar expect for main screeps item
    DEV-274

--- src/consumed-services/consume-status-bar.ts
+++ src/consumed-services/consume-status-bar.ts
@@ -5,8 +5,6 @@ import { tap } from 'rxjs/operators';
 import { default as __state } from '../state';
 
 import { MODULES_URI } from '../components/modules-pane';
-import { CONSOLE_URI } from '../components/console-panel';
-import { MEMORY_URI } from '../components/memory-panel';
 
 import { ScreepsStatusBar } from '../components/screeps-status-bar';
 
@@ -15,35 +13,12 @@ const priority = 10000;
 export function consumeStatusBar(statusBar: any) {
     const subscriptions = new CompositeDisposable();
 
-
     const consoleStatusBar = new ScreepsStatusBar(__state);
     // Rerender compoment if branch changes in ModulesView 
     __state.pipe(tap((state: IState) => consoleStatusBar.render(state)))
         .subscribe();
     statusBar.addLeftTile({ item: consoleStatusBar.element, priority });
-
-
-    const consoleElementRef = document.createElement('div');
-    consoleElementRef.innerText = 'Console';
-    consoleElementRef.classList.add('screeps-ide__status-bar', 'inline-block');
-    consoleElementRef.addEventListener('click', () => atom.workspace.open(CONSOLE_URI, {
-        activatePane: true,
-        activateItem: true,
-        // split: 'down',
-        location: 'bottom'
-    }));
-
-    statusBar.addLeftTile({
-        item: consoleElementRef,
-        priority
-    });
-    subscriptions.add(atom.tooltips.add(consoleElementRef, { title: 'Show Console '}));
-
-
-    const modulesElementRef = document.createElement('div');
-    modulesElementRef.innerText = 'Modules';
-    modulesElementRef.classList.add('screeps-ide__status-bar', 'inline-block');
-    modulesElementRef.addEventListener('click', () => atom.workspace.open(MODULES_URI, {
+    consoleStatusBar.element.addEventListener('click', () => atom.workspace.open(MODULES_URI, {
         activatePane: true,
         activateItem: true,
         // split: 'down',
@@ -51,25 +26,8 @@ export function consumeStatusBar(statusBar: any) {
     }));
 
     statusBar.addLeftTile({
-        item: modulesElementRef,
+        item: consoleStatusBar.element,
         priority
     });
-    subscriptions.add(atom.tooltips.add(modulesElementRef, { title: 'Show Modules '}));
-
-    const memoryElementRef = document.createElement('div');
-    memoryElementRef.innerText = 'Memory';
-    memoryElementRef.classList.add('screeps-ide__status-bar', 'inline-block');
-    // memoryElementRef.addEventListener('click', () => showMemoryPanelCommand());
-    memoryElementRef.addEventListener('click', () => atom.workspace.open(MEMORY_URI, {
-        activatePane: true,
-        activateItem: true,
-        // split: 'down',
-        location: 'bottom'
-    }));
-
-    statusBar.addLeftTile({
-        item: memoryElementRef,
-        priority
-    });
-    subscriptions.add(atom.tooltips.add(memoryElementRef, { title: 'Show Memory '}));
+    subscriptions.add(atom.tooltips.add(consoleStatusBar.element, { title: 'Show screeps panel'}));
 }
