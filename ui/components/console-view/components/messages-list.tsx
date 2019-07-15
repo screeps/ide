a5618c68651d2dd28342c42367291bbbaac3b1aa
DmitriyFF Smile <dmitriyff@gmail.com>
2019-07-16 06:41:09 +0700

    refactor(ide): Refactor console view messages
    DEV-274

--- ui/components/console-view/components/messages-list.tsx
+++ ui/components/console-view/components/messages-list.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useRef, useEffect } from 'react';
 
 import { default as ConsoleMessageItemView } from './message-item';
 
@@ -6,38 +7,24 @@ interface IConsoleMessagesListViewProps {
     messages: any[]
 }
 
-interface IConsoleMessagesListViewState {
+export default function({ messages }: IConsoleMessagesListViewProps) {
+    const messagesBottomRef = useRef(null)
+
+    useEffect(() => {
+        if (!messagesBottomRef.current) {
+            return;
+        }
+
+        // @ts-ignore
+        messagesBottomRef.current.scrollIntoView({ behavior: 'smooth' });
+    }, [messages]);
+
+    return (
+        <div className='screeps-console__messages'>
+            {messages.map((message, index) => {
+                return (<ConsoleMessageItemView key={ index } message={ message } />)
+            })}
+            <div ref={ messagesBottomRef } />
+        </div>
+    );
 }
-
-class ConsoleMessagesListView extends React.Component<IConsoleMessagesListViewProps> {
-    //@ts-ignore
-    props: IConsoleMessagesListViewProps;
-
-    state: IConsoleMessagesListViewState;
-
-    public messagesBottomRef: HTMLDivElement | null = null;
-
-    constructor(props: IConsoleMessagesListViewProps) {
-        super(props);
-
-        this.state = {};
-    }
-
-    public render() {
-        return (
-            <div className='screeps-console__messages'>
-                {this.props.messages.map((message, index) => {
-                    return (<ConsoleMessageItemView key={ index } message={ message } />)
-                })}
-                <div ref={(el) => this.messagesBottomRef = el} />
-            </div>
-        );
-    }
-
-    componentDidUpdate() {
-        //@ts-ignore
-        this.messagesBottomRef.scrollIntoView({ behavior: "smooth" });
-    }
-}
-
-export default ConsoleMessagesListView;
