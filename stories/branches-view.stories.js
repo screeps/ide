cea292d4993b9d8a103e4a6f108da17dcb6d49c4
DmitriyFF Smile <dmitriyff@gmail.com>
2019-07-15 21:27:57 +0700

    feat(ide/stories): Add branches view story
    DEV-274

---
+++ stories/branches-view.stories.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import { default as BranchesView } from '../ui/components/branches-view';
+
+const branch = 'default';
+const branches = [{
+    _id: '1',
+    branch: 'default',
+    activeWorld: true
+}, {
+    _id: '2',
+    branch: 'simulation',
+    activeSim: true
+}, {
+    _id: '3',
+    branch: 'tutorial'
+}];
+
+storiesOf('UI Components|Branches View', module)
+    .add('View', () => (
+        <atom-dock>
+            <BranchesView
+                branch={ branch }
+                branches={ branches }
+            />
+        </atom-dock>
+    ));
