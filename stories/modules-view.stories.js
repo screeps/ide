c2843200db029fefa283bc845c86886db2461b73
DmitriyFF Smile <dmitriyff@gmail.com>
2019-07-15 21:38:00 +0700

    feat(ide/storybook): Add modules view to storybook
    DEV-274

---
+++ stories/modules-view.stories.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import { default as ModulesView } from '../ui/components/modules-view';
+
+const branch = 'default';
+const modules = [];
+
+storiesOf('UI Components|Modules View', module)
+    .add('View', () => (
+        <atom-dock>
+            <ModulesView
+                branch={ branch }
+                modules={ modules }
+            />
+        </atom-dock>
+    ));
