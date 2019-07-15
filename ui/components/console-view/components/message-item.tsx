a358bf5fb8f3c99f6d44e07e8a1705e67c15ed5a
DmitriyFF Smile <dmitriyff@gmail.com>
2019-07-16 06:45:02 +0700

    refactor(ide/console): Refactor console message item
    DEV-274

--- ui/components/console-view/components/message-item.tsx
+++ ui/components/console-view/components/message-item.tsx
@@ -8,69 +8,61 @@ function unescapeHTML(escapedHTML: string) {
     return escapedHTML.replace(/&lt;/g,'<').replace(/&gt;/g,'>').replace(/&amp;/g,'&');
 }
 
-class ConsoleMessageItemView extends React.Component<IConsoleMessageItemViewProps> {
-    constructor(props: IConsoleMessageItemViewProps) {
-        super(props);
-    }
+export default function(props: IConsoleMessageItemViewProps) {
+    let timestamp, shard, message, error;
 
-    public render() {
-        let timestamp, shard, message, error;
-
-        if (this.props.message.timeStamp) {
-            timestamp = (
-                <span className='--timestamp'>
-                    <span className='brackets'>
-                        { new Date(this.props.message.timeStamp) .toLocaleTimeString() }
-                    </span>
-                    &nbsp;
+    if (props.message.timeStamp) {
+        timestamp = (
+            <span className='--timestamp'>
+                <span className='brackets'>
+                    { new Date(props.message.timeStamp) .toLocaleTimeString() }
                 </span>
-            );
-        }
-
-        if (this.props.message.shard) {
-            shard = (
-                <span>
-                    <span className='brackets'>
-                        { this.props.message.shard }
-                    </span>
-                    &nbsp;
-                </span>
-            );
-        }
-
-        if (this.props.message.log) {
-            message = (
-                <span className='--log'>{ unescapeHTML(this.props.message.log) }</span>
-            );
-        }
-
-        if (this.props.message.expression) {
-            message = (
-                <span className='--input'>{ unescapeHTML(this.props.message.expression) }</span>
-            );
-        }
-
-        if (this.props.message.result) {
-            message = (
-                <span className='--output'>{ unescapeHTML(this.props.message.result) }</span>
-            );
-        }
-
-        if (this.props.message.error) {
-            error =(
-                <span className='--error'>{ unescapeHTML(this.props.message.error) }</span>
-            );
-        }
-
-        return (
-            <div className='screeps-console__message native-key-bindings' tabIndex={ -1 }>
-                { timestamp }
-                { shard }
-                { message }
-                { error }
-            </div>
+                &nbsp;
+            </span>
         );
     }
-}
 
-export default ConsoleMessageItemView;
+    if (props.message.shard) {
+        shard = (
+            <span>
+                <span className='brackets'>
+                    { props.message.shard }
+                </span>
+                &nbsp;
+            </span>
+        );
+    }
+
+    if (props.message.log) {
+        message = (
+            <span className='--log'>{ unescapeHTML(props.message.log) }</span>
+        );
+    }
+
+    if (props.message.expression) {
+        message = (
+            <span className='--input'>{ unescapeHTML(props.message.expression) }</span>
+        );
+    }
+
+    if (props.message.result) {
+        message = (
+            <span className='--output'>{ unescapeHTML(props.message.result) }</span>
+        );
+    }
+
+    if (props.message.error) {
+        error =(
+            <span className='--error'>{ unescapeHTML(props.message.error) }</span>
+        );
+    }
+
+    return (
+        <div className='screeps-console__message native-key-bindings' tabIndex={ -1 }>
+            { timestamp }
+            { shard }
+            { message }
+            { error }
+        </div>
+    );
+}
