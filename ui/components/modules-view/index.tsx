db8c36c5d5325d0380fdf5320b8e859b028aca99
DmitriyFF Smile <dmitriyff@gmail.com>
2019-08-01 22:52:51 +0700

    refactor(ide/modules): Refactor modules view
    DEV-274

--- ui/components/modules-view/index.tsx
+++ ui/components/modules-view/index.tsx
@@ -4,90 +4,61 @@ import * as React from 'react';
 
 const MAIN_MODULE = 'main';
 
-class ModulesView extends React.Component<IModulesViewProps> {
-    //@ts-ignore
-    props: IModulesViewProps;
+export default function(props: IModulesViewProps) {
 
-    constructor(props: IModulesViewProps) {
-        super(props);
-
-        // console.log('ModulesView::constructor', props.modules);
-    }
-
-    public render() {
-        // console.log('ModulesView::render', '');
-
-        return (
-            <div className='screeps-ide screeps-modules-view'>
-                <div className='screeps-modules-view__header'>
-                    <span>Branch</span>
-                    <span>{ this.props.branch }</span>
-                </div>
-                <hr className={ 'screeps-hr' } />
-                <div className='screeps-modules-view__items'>
-                    <ul className='tab-bar'>
-                        <li className={ 'tab screeps-modules-view__item screeps-modules-view__module' + (
-                            this.props.modules[MAIN_MODULE] && this.props.modules[MAIN_MODULE].modified ? ' modified' : '' ) +  (
-                            this.props.modules[MAIN_MODULE] && this.props.modules[MAIN_MODULE].active ? ' active' : ''
-                        )}>
-                            <button className='btn btn--clear' onClick={() => this.onSelectModule(MAIN_MODULE)}>{ MAIN_MODULE }</button>
-                            <div className='modified-icon'></div>
-                        </li>
-                        {this._getAdditionalModules(this.props.modules).map(([ moduleName, { modified, active }]) => {
-                            return (
-                        <li className={ 'tab screeps-modules-view__item screeps-modules-view__module' + (
-                            modified ? ' modified' : '' ) +  (
-                            active ? ' active' : ''
-                        )} key={moduleName}>
-                            <button className='btn btn--clear' onClick={() => this.onSelectModule(moduleName)}>{ moduleName }</button>
-                            <div className='close-icon' onClick={() => this.onDeleteModule(moduleName)}></div>
-                        </li>
-                            );
-                        })}
-                    </ul>
-                </div>
-                <div className='screeps-modules-view__new'>
-                    <form>
-                        <fieldset className='screeps-field'>
-                            <input className='native-key-bindings' type='text' placeholder='New module name...'
-
-                                autoComplete=''
-
-                                onKeyPress={ this.onKeyPressHandler }/>
-                            <div className='underline' />
-                        </fieldset>
-                    </form>
-                </div>
+    return (
+        <div className='screeps-ide screeps-modules-view'>
+            <div className='screeps-modules-view__header'>
+                <span>Branch</span>
+                <span>{ props.branch }</span>
             </div>
-        );
+            <hr className={ 'screeps-hr' } />
+            <div className='screeps-modules-view__items'>
+                <ul className='tab-bar'>
+                    <li className={ 'tab screeps-modules-view__item screeps-modules-view__module' + (
+                        props.modules[MAIN_MODULE] && props.modules[MAIN_MODULE].modified ? ' modified' : '' ) +  (
+                        props.modules[MAIN_MODULE] && props.modules[MAIN_MODULE].active ? ' active' : ''
+                    )}>
+                        <button className='btn btn--clear' onClick={() => onSelectModule(MAIN_MODULE)}>{ MAIN_MODULE }</button>
+                        <div className='modified-icon'></div>
+                    </li>
+                    {_getAdditionalModules(props.modules).map(([ moduleName, { modified, active }]) => {
+                        return (
+                    <li className={ 'tab screeps-modules-view__item screeps-modules-view__module' + (
+                        modified ? ' modified' : '' ) +  (
+                        active ? ' active' : ''
+                    )} key={moduleName}>
+                        <button className='btn btn--clear' onClick={() => onSelectModule(moduleName)}>{ moduleName }</button>
+                        <div className='close-icon' onClick={() => onDeleteModule(moduleName)}></div>
+                    </li>
+                        );
+                    })}
+                </ul>
+            </div>
+            <div className='screeps-modules-view__new'>
+                <form>
+                    <fieldset className='screeps-field'>
+                        <input className='native-key-bindings' type='text' placeholder='New module name...'
+
+                            autoComplete=''
+
+                            onKeyPress={ onKeyPressHandler }/>
+                        <div className='underline' />
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+    );
+
+    function onSelectModule(module: string) {
+        props.onSelectModule && props.onSelectModule(module);
     }
 
-    hasChanges() {
-        return Object.values(this.props.modules)
-            .some(({ modified, deleted }) => !!modified || !!deleted);
+    function onDeleteModule(module: string) {
+        props.onDeleteModule && props.onDeleteModule(module);
     }
 
-    onCreateModule(module: string) {
-        this.props.onCreateModule && this.props.onCreateModule(module);
-    }
-
-    onSelectModule(module: string) {
-        this.props.onSelectModule && this.props.onSelectModule(module);
-    }
-
-    onDeleteModule(module: string) {
-        this.props.onDeleteModule && this.props.onDeleteModule(module);
-    }
-
-    onApplyChanges() {
-        this.props.onApplyChanges && this.props.onApplyChanges();
-    }
-
-    onRevertChanges() {
-        this.props.onRevertChanges && this.props.onRevertChanges();
-    }
-
-    onKeyPressHandler = (event: any) => {
+    function onKeyPressHandler(event: any) {
         if (event.key !== 'Enter') {
             return;
         }
@@ -96,18 +67,16 @@ class ModulesView extends React.Component<IModulesViewProps> {
             return;
         }
 
-        this.props.onCreateModule && this.props.onCreateModule(event.target.value);
+        props.onCreateModule && props.onCreateModule(event.target.value);
 
         event.target.value = '';
     }
 
-    private _getAdditionalModules = (modules: {
+    function _getAdditionalModules(modules: {
         [key: string]: IModule;
-    }) => {
+    }) {
         return Object.entries(modules).filter(([name, { deleted }]) => {
             return name !== MAIN_MODULE && !deleted;
         });
     }
 }
-
-export default ModulesView;
