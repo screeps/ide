ddbbc53bf557a0b7216507bafd760fbe073ca1a2
DmitriyFF Smile <dmitriyff@gmail.com>
2019-08-01 21:05:47 +0700

    feat(ide/branches): Add abilitu to scroll branches
    DEV-274

--- ui/components/branches-view/index.tsx
+++ ui/components/branches-view/index.tsx
@@ -21,48 +21,50 @@ export default class BranchesView extends React.Component<IBranchesViewProps> {
                     <span>Branches</span>
                 </div>
                 <hr className='screeps-hr' />
-                <ul className='tab-bar screeps-branches-view__items'>
-                {this.props.branches && this.props.branches.map(({ _id, branch, activeSim, activeWorld }) => {
-                    let deleteButton;
-                    let sim;
-                    let world;
+                <div className='screeps-branches-view__items'>
+                    <ul className='tab-bar'>
+                    {this.props.branches && this.props.branches.map(({ _id, branch, activeSim, activeWorld }) => {
+                        let deleteButton;
+                        let sim;
+                        let world;
 
-                    if (!activeSim && !activeWorld) {
-                        deleteButton = <div className='close-icon' onClick={() => this.onDeleteBranch(branch)}></div>;
-                    }
+                        if (!activeSim && !activeWorld) {
+                            deleteButton = <div className='close-icon' onClick={() => this.onDeleteBranch(branch)}></div>;
+                        }
 
-                    if (activeWorld) {
-                        world = (<button className='screeps-branches-view__world --active'>
-                            world
-                        </button>);
-                    } else {
-                        world = (<button className='screeps-branches-view__world'
-                            onClick={() => this.onSetActiveWorld(branch)}>
-                            world
-                        </button>);
-                    }
+                        if (activeWorld) {
+                            world = (<button className='screeps-branches-view__world --active'>
+                                world
+                            </button>);
+                        } else {
+                            world = (<button className='screeps-branches-view__world'
+                                onClick={() => this.onSetActiveWorld(branch)}>
+                                world
+                            </button>);
+                        }
 
-                    if (activeSim) {
-                        sim = (<button className='screeps-branches-view__sim --active'>
-                            sim
-                        </button>);
-                    } else {
-                        sim = (<button className='screeps-branches-view__sim'
-                            onClick={() => this.onSetActiveSim(branch)}>
-                            sim
-                        </button>);
-                    }
+                        if (activeSim) {
+                            sim = (<button className='screeps-branches-view__sim --active'>
+                                sim
+                            </button>);
+                        } else {
+                            sim = (<button className='screeps-branches-view__sim'
+                                onClick={() => this.onSetActiveSim(branch)}>
+                                sim
+                            </button>);
+                        }
 
-                    return (
-                        <li className='tab screeps-branches-view__item' key={_id}>
-                            <button className='btn btn--clear' onClick={() => this.onCopyBranch(branch)}><i className='sc-icon-copy' /></button>
-                            <button className='btn btn--clear' onClick={() => this.onSelectBranch(branch)}>{ branch }</button>
-                            { world } { sim }
-                            { deleteButton }
-                        </li>
-                    )
-                })}
-                </ul>
+                        return (
+                            <li className='tab screeps-branches-view__item' key={_id}>
+                                <button className='btn btn--clear' onClick={() => this.onCopyBranch(branch)}><i className='sc-icon-copy' /></button>
+                                <button className='btn btn--clear' onClick={() => this.onSelectBranch(branch)}>{ branch }</button>
+                                { world } { sim }
+                                { deleteButton }
+                            </li>
+                        )
+                    })}
+                    </ul>
+                </div>
             </div>
         );
     }
