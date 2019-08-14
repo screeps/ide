9e39293bc08c88f7f7ecb3a14a8875a44ae73c4a
DmitriyFF Smile <dmitriyff@gmail.com>
2019-08-14 14:19:47 +0700

    feat(ide/console):Block auto scroll to bottom if messages scrolled
    DEV-274

--- ui/components/console-view/components/messages-list.tsx
+++ ui/components/console-view/components/messages-list.tsx
@@ -7,11 +7,13 @@ interface IConsoleMessagesListViewProps {
     messages: any[]
 }
 
+let scrollToBottom = true;
 export default function({ messages }: IConsoleMessagesListViewProps) {
-    const messagesBottomRef = useRef(null)
+    const messagesRef = useRef<HTMLDivElement>(null);
+    const messagesBottomRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-        if (!messagesBottomRef.current) {
+        if (!messagesBottomRef.current || !scrollToBottom) {
             return;
         }
 
@@ -20,11 +22,23 @@ export default function({ messages }: IConsoleMessagesListViewProps) {
     }, [messages]);
 
     return (
-        <div className='screeps-console__messages'>
+        <div className='screeps-console__messages' ref={ messagesRef }
+            onScroll={ onScroll }
+        >
             {messages.map((message, index) => {
                 return (<ConsoleMessageItemView key={ index } message={ message } />)
             })}
             <div ref={ messagesBottomRef } />
         </div>
     );
+
+    function onScroll() {
+        if (!messagesRef.current) {
+            return;
+        }
+
+        const { offsetHeight, scrollHeight, scrollTop } = messagesRef.current;
+
+        scrollToBottom = scrollHeight === (scrollTop + offsetHeight);
+    }
 }
