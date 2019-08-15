5ca1f2c1690575f19dba18b84085bb7e5fe7096e
DmitriyFF Smile <dmitriyff@gmail.com>
2019-08-16 02:06:33 +0700

    feat(ide/storybook): Add links on welcome to other stories
    DEV-274

--- stories/welcome-view.stories.js
+++ stories/welcome-view.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
+import { linkTo } from '@storybook/addon-links';
 
 import { default as WelcomeView } from '../ui/components/welcome-view';
 
@@ -8,6 +9,8 @@ storiesOf('UI Components|Welcome View', module)
     .add('View', () => (
         <WelcomeView
             showOnStartup={ true }
+            onSignin={ linkTo('UI Components|Auth View', 'Clear') }
+            onCreateNewProject={ linkTo('UI Components|Create Project View', 'Create') }
             onChangeShowOnStartup={() => {}}
         />
     ));
