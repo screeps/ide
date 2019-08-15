e69def7fd9f4a323e36eaa6a36a49d9e43ed67f5
DmitriyFF Smile <dmitriyff@gmail.com>
2019-08-16 05:10:07 +0700

    refactor(ide/config): Remove obsoleted local config properties
    DEV-274

--- src/config.ts
+++ src/config.ts
@@ -1,90 +1,12 @@
-const fs = require('fs');
-const path = require('path');
-
 interface IAtomConfig {
     showOnStartup: any;
     showProjectConfig: any;
     global: any;
-    local?: any;
 }
-
-interface IAtomConfigProperties {
-    [key: string]: object;
-}
-
-let local: {
-    properties: IAtomConfigProperties;
-};
-
-const properties: IAtomConfigProperties = {
-    authToken: {
-        title: 'Auth token',
-        description: '',
-        type: 'string',
-        default: '',
-        order: 1
-    },
-
-    apiUrl: {
-        title: 'Screep API url',
-        description: '',
-        type: 'string',
-        default: '',
-        order: 1
-    },
-
-    websocketUrl: {
-        title: 'Screep websocket Url',
-        description: '',
-        type: 'string',
-        default: '',
-        order: 1
-    },
-
-    src: {
-        title: 'Project folder',
-        description: '',
-        type: 'string',
-        default: '',
-        order: 1
-    }
-};
 
 export const PACKAGE_NAME = 'screeps-ide';
-const LOCAL_PROJECT_CONFIG = '.screepsiderc'
 
-try {
-    atom.config.unset(`${ PACKAGE_NAME }.local`);
-
-    const paths = atom.project.getPaths();
-    const _path = path.resolve(paths[0], LOCAL_PROJECT_CONFIG);
-
-    const _buffer = fs.readFileSync(_path);
-    const _config = JSON.parse(_buffer.toString());
-
-    for (let prop in _config) {
-        if (!_config[prop] || !properties[prop]) {
-            continue;
-        }
-
-        //@ts-ignore
-        local = local || {
-            title: 'Per-project',
-            type: 'object',
-            order: 1,
-            properties: {}
-        };
-
-        local.properties[prop] = properties[prop];
-
-        atom.config.set(`${ PACKAGE_NAME }.local.${ prop }`, _config[prop]);
-    }
-
-} catch(err) {
-    // Noop.
-}
-
-const _global = {
+const global = {
     title: 'Common',
     type: 'object',
     order: 1,
@@ -136,13 +58,8 @@ let config: IAtomConfig = {
         description: 'Show config .screepsiderc in local projects.'
     },
 
-    global: _global
+    global
 };
-
-//@ts-ignore
-if (local) {
-    config.local = local;
-}
 
 export default config;
 
